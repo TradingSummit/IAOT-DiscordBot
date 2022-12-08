98c30e10316555137b8802b93f0f1a245291cb22
bence <75566095+31b4@users.noreply.github.com>
2022-12-08 15:02:19 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -9,10 +9,33 @@ from discord import Client, Intents, Interaction, app_commands
 #from forex_python.bitcoin import BtcConverter
 from discord.ext import commands, tasks
 
-intents = discord.Intents(messages=True, guilds=True)
+intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='$', intents=intents,allowed_mentions = discord.AllowedMentions(everyone = True))
-
+client = discord.Client( intents=intents)
+@client.event
+async def on_message(message):
+    print(message.content)
+    msg = ""
+    reaction= ""
+    if 'in' == message.content.lower():
+      msg = "and **Out**"
+      reaction = "<:IAOTLogo:1045015878678360084>" 
+    elif len([x for x in ['niga',"nigger","nigga","niger","nig", "black monkey"] if x in message.content.lower()])>0:
+      msg = ""
+      reaction = "👍🏿"
+    elif 'ping' == message.content.lower():
+      msg = "**pong**"
+    elif '<@1017757886820134963>' == message.content.lower():
+      msg = "**is a Monke**"
+    elif len([x for x in ['help'] if x in message.content.lower()])>0:
+      msg = "<@&1043655353457451038>"
+    
+    if msg != "":
+      await message.reply(msg)
+    if reaction != "":
+      await message.add_reaction(reaction)
+        
+bot = commands.Bot(command_prefix='', intents=intents,allowed_mentions = discord.AllowedMentions(everyone = True))
 def check_me(token_test: str) -> dict:
     r = requests.get("https://discord.com/api/v10/users/@me", headers={
         "Authorization": f"Bot {token_test}"
@@ -33,96 +56,47 @@ async def on_ready():
         f"Use this URL to invite {bot.user} to your server:",
         f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&scope=applications.commands%20bot"
     ]))
-    checkforvideos.start()
+    #checkforvideos.start()
     
-class FunnyBadge(Client):
-    def __init__(self, *, intents: Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
 
-    async def setup_hook(self) -> None:
-        await self.tree.sync(guild=None)
-
-client = FunnyBadge(intents=Intents.none())
 
 @bot.tree.command()
 async def hello(interaction: Interaction):
-    """ In and Out Trading """
-    print(f"> {interaction.user} used the command.")
-    await interaction.response.send_message("\n".join([
-      f"Hi **{interaction.user}**, thank you for saying hello to me.",
-      "",
-      "__**What is In and Out?**__",
-      "<:inandoutlogofekete:1014902819079344179> In And Out Trading  is a trading team where we help each other in trading and discuss our ideas about the markets! <:inandoutlogofekete:1014902819079344179>",
-      "",
-      "<:RoleSupport:1035899393087373392> Our goal is to have a disciplined community, and build relationships <:RoleSupport:1035899393087373392>",
-      "",
-      f"<:inandoutlogofeher:1014902792411938926>You can find more info about our channels in <#{985238159480746034}> <:inandoutlogofeher:1014902792411938926>"
+  """ In and Out Trading """
+  print(f"> {interaction.user} used the command.")
+  await interaction.response.send_message("\n".join([
+    f"Hi **<@{interaction.user.id}>**, thank you for saying **hello** to me.",
+    "",
+    "__**What is In and Out?**__",
+    "<:IAOTLogo:1045015878678360084> **In And Out Trading**  is a trading team where we help each other in trading and discuss our ideas about the markets! <:IAOTLogo:1045015878678360084>",
+    "",
+    "<:rp:1044260758449557534> Our goal is to have a **disciplined** community, and build relationships <:rp:1044260758449557534>",
+    "",
+    f"<:illuminati:1046868311260020736>You can find more info about our channels in <#{1043655425637240853}> <:illuminati:1046868311260020736>",
+    "",
+    f":man_mage:Start **chatting** in <#{1043655431135957032}> :man_mage:"
+    
+  ]))
+  
+@bot.tree.command()
+async def hey(interaction: Interaction):
+  """ In and Out Trading """
+  print(f"> {interaction.user} used the command.")
+  await interaction.response.send_message("\n".join([
+    f"Hi **<@{interaction.user.id}>**, thank you for saying **hello** to me.",
+    "",
+    "__**What is In and Out?**__",
+    "<:IAOTLogo:1045015878678360084> **In And Out Trading**  is a trading team where we help each other in trading and discuss our ideas about the markets! <:IAOTLogo:1045015878678360084>",
+    "",
+    "<:rp:1044260758449557534> Our goal is to have a **disciplined** community, and build relationships <:rp:1044260758449557534>",
+    "",
+    f"<:illuminati:1046868311260020736>You can find more info about our channels in <#{1043655425637240853}> <:illuminati:1046868311260020736>",
+    "",
+    f":man_mage:Start **chatting** in <#{1043655431135957032}> :man_mage:"
       
   ]))
 
-@tasks.loop(seconds=10)
-async def checkforvideos():
-  print("Now Checking!")
-  with open("youtubedata.json", "r") as f:
-    data=json.load(f)
-  #printing here to show
 
-  #checking for all the channels in youtubedata.json file
-  for youtube_channel in data:
-    #getting youtube channel's url
-    channel = f"https://www.youtube.com/channel/{youtube_channel}"
-    #getting html of the /videos page
-    html = requests.get(channel+"/videos").text
-    #getting the latest video's url
-    #put this line in try and except block cause it can give error some time if no video is uploaded on the channel
-    videos = scrapetube.get_channel("UCRqber9SjCK7TtX_PqV87Pg")
-  
-    for video in videos:
-      latest_video_url ="https://www.youtube.com/watch?v="+video['videoId']
-      break
-
-    #checking if url in youtubedata.json file is not equals to latest_video_url
-    if not str(data[youtube_channel]["latest_video_url"]) == latest_video_url:
-
-      #changing the latest_video_url
-      data[str(youtube_channel)]['latest_video_url'] = latest_video_url
-
-      #dumping the data
-      with open("youtubedata.json", "w") as f:
-        json.dump(data, f)
-
-      #getting the channel to send the message
-      discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
-      discord_channel = bot.get_channel(int(discord_channel_id))
-
-      #sending the msg in discord channel
-      #you can mention any role like this if you want
-      msg = f"@everyone **{data[str(youtube_channel)]['nick_name']}** Just Uploaded A Video! Go Check It Out: {latest_video_url}"
-      #if you'll send the url discord will automaitacly create embed for it
-      #if you don't want to send embed for it then do <{latest_video_url}>
-
-      await discord_channel.send(msg)
-      
-#creating command to add more youtube accounds data in youtubedata.json file
-#you can also use has_role if you don't want to allow everyone to use this command
-@bot.command()
-@commands.has_role("Youtube")
-async def add_youtube_notification_data(ctx, channel_id: str, *, channel_name: str):
-  with open("youtubedata.json", "r") as f:
-    data = json.load(f)
-  
-  data[str(channel_id)]={}
-  data[str(channel_id)]["channel_name"]=channel_name
-  data[str(channel_id)]["latest_video_url"]="none"
-
-  #you can also get discord_channe id from the command 
-  #but if the channel is same then you can also do directly
-  data[str(channel_id)]["notifying_discord_channel"]="1041300944039718922"
-
-  with open("youtubedata.json", "w") as f:
-    json.dump(data, f)
-  await ctx.send("Added Your Account Data!")
+client.run("ODgyMjQxNjk1MjkzNTI2MTE2.GCIZCE.1XcsiSmRAAmmfcRoCB895B_1Fe_BLNsnS0QNLw")
 
 
-bot.run("asd")
